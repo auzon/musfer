3be5444b79e3b5fb5ba71406d3404c7aacc462bf
auzon <oguzhanozer90@outlook.com>
2025-12-19 21:31:02 +0300

    chore: multirepo to monorepo track-service

---
+++ track-service/src/main/java/me/oguzhanozer/musfer/trackservice/TrackserviceApplication.java
@@ -0,0 +1,13 @@
+package me.oguzhanozer.musfer.trackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackserviceApplication.class, args);
+	}
+
+}
